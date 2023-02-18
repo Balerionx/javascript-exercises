51e5ba473f6d3400d081e4627371a0b6806744a4
Balerionx <jovan.usendic@gmail.com>
2023-02-18 18:09:20 +0100

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr, ...value) {
+const values = value;
+return arr = arr.filter(item => !values.includes(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
