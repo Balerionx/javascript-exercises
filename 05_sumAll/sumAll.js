1c28252a63af6c2b6d57c7c15363f68131cd2232
Balerionx <jovan.usendic@gmail.com>
2023-02-26 14:33:04 +0100

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+   if (a < 0 || typeof a !== 'number' || b < 0 || typeof b !== 'number') {
+    return "ERROR"; 
+} else if (a > b) {
+  [a, b] = [b, a];
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
+ } else if (a < b) {
+ let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
+}
 };
 
 // Do not edit below this line
