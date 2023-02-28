89a03fe8a8ccce63f1d4a957f076a5c69e357f9b
Balerionx <jovan.usendic@gmail.com>
2023-02-28 15:34:36 +0100

    Completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(c, d) {
+	return c-d;
 };
 
-const sum = function() {
-	
+const sum = function(e) {
+  let f = 0;
+  for (let i = 0; i < e.length; i++) {
+    f += e[i];
+  }
+  return f;
 };
 
-const multiply = function() {
-
+const multiply = function(g) {
+  let h = 1;
+  for (let i = 0; i < g.length; i++) {
+    h = h*g[i];
+  }
+  return h;
 };
 
-const power = function() {
-	
+const power = function(i, j) {
+	return i ** j;
 };
 
-const factorial = function() {
-	
+const factorial = function(k) {
+	let l = 1;
+  for (let i = k; i >=1; i--) {
+    l = l*i;
+  }
+  return l;
 };
 
 // Do not edit below this line
