018ffd527fe2e2273b8dc1cd78912bc022bb2939
Balerionx <jovan.usendic@gmail.com>
2023-03-04 07:54:36 +0100

    Finished 09, used regex for the first time

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/[^a-z0-p]/g, '');
+  const reverseStr = str.toLowerCase().split('').reverse().join('');
+  if (str === reverseStr) {
+    return true;
+  } {return false;}
 };
 
 // Do not edit below this line
